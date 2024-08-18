18a98070a214e5a152c791d8b100b1f5dd9b870a
Josué Júnior <josue.005.junior@gmail.com>
2024-08-18 16:55:37 -0300

    1.2

--- Scripts/CharacterMovement.cs
+++ Scripts/CharacterMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public struct CharacterMovementInput
 {
     public Vector2 MoveInput;
+    public bool WantsToJump;
 }
 
 [RequireComponent(typeof(KinematicCharacterMotor))]
@@ -11,11 +12,22 @@ public class CharacterMovement : MonoBehaviour, ICharacterController
 {
     public KinematicCharacterMotor Motor;
 
-    public float MaxSpeed;
-    public float Acceleration;
-    public float RotationSpeed;
+    [Header("Ground Movement")] public float MaxSpeed = 5;
+    public float Acceleration = 50;
+    public float RotationSpeed = 15;
+    public float Gravity = 30;
+    public float JumpHeight = 1.5f;
+    [Range(0.01f, 0.3f)] public float JumpRequestDuration = 0.1f;
+
+    [Header("Air Movement")] public float AirMaxSpeed = 3;
+
+    public float AirAcceleration = 20;
+    [Min(0)] public float Drag = 0.5f;
 
     private Vector3 moveInput;
+    private float jumpRequestExpireTime;
+
+    public float JumpSpeed => Mathf.Sqrt(2 * Gravity * JumpHeight);
 
     private void Awake()
     {
@@ -28,6 +40,11 @@ public class CharacterMovement : MonoBehaviour, ICharacterController
         if (input.MoveInput != Vector2.zero)
         {
             moveInput = new Vector3(input.MoveInput.x, 0, input.MoveInput.y).normalized;
+        }
+
+        if (input.WantsToJump)
+        {
+            jumpRequestExpireTime = Time.time + JumpRequestDuration;
         }
     }
 
@@ -42,9 +59,38 @@ public class CharacterMovement : MonoBehaviour, ICharacterController
 
     public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime)
     {
-        var targetVelocity = moveInput * MaxSpeed;
-        // currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, Acceleration * deltaTime);
-        currentVelocity = Vector3.MoveTowards(currentVelocity, targetVelocity, Acceleration * deltaTime);
+        //grounded
+        if (Motor.GroundingStatus.IsStableOnGround)
+        {
+            var targetVelocity = moveInput * MaxSpeed;
+            // currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, Acceleration * deltaTime);
+            currentVelocity = Vector3.MoveTowards(currentVelocity, targetVelocity, Acceleration * deltaTime);
+
+            if (Time.time < jumpRequestExpireTime)
+            {
+                currentVelocity.y = JumpSpeed;
+                jumpRequestExpireTime = 0;
+                Motor.ForceUnground();
+            }
+        }
+        //air movement
+        else
+        {
+            var targetVelocityXZ = new Vector2(moveInput.x, moveInput.z) * AirMaxSpeed;
+            var currentVelocityXZ = new Vector2(currentVelocity.x, currentVelocity.z);
+
+            currentVelocityXZ = Vector2.MoveTowards(currentVelocityXZ, targetVelocityXZ, AirAcceleration * deltaTime);
+
+            currentVelocity.x = ApplyDrag(currentVelocityXZ.x, Drag, deltaTime);
+            currentVelocity.z = ApplyDrag(currentVelocityXZ.y, Drag, deltaTime);
+
+            currentVelocity.y -= Gravity * deltaTime;
+        }
+    }
+
+    private static float ApplyDrag(float v, float drag, float deltaTime)
+    {
+        return v * (1f / (1f + drag * deltaTime));
     }
 
     #region not implemented
@@ -87,4 +133,4 @@ public class CharacterMovement : MonoBehaviour, ICharacterController
     }
 
     #endregion
-} 
+}
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,20 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using KinematicCharacterController;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-
 {
     [SerializeField] private CharacterMovement characterMovement;
     private void Update()
     {
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
+        var wantsToJump = Input.GetKeyDown(KeyCode.Space);
         
         characterMovement.SetInput(new CharacterMovementInput()
         {
-            MoveInput = new Vector2(h, v)
+            MoveInput = new Vector2(h, v),
+            WantsToJump = wantsToJump
         });
     }
 }
