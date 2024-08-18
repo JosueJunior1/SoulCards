168bc29a7ab9ae8c8f2ff8d69850d8ee83bcac5b
Josué Júnior <josue.005.junior@gmail.com>
2024-08-18 14:52:03 -0300

    1.0

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using KinematicCharacterController;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+    [SerializeField] private CharacterMovement characterMovement;
+    private void Update()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        
+        characterMovement.SetInput(new CharacterMovementInput()
+        {
+            MoveInput = new Vector2(h, v)
+        });
+    }
+}
